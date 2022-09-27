4cab5f6a7a211cbf93c95355225d36a58fb34021
MohsenSami <mohsensami88@gmail.com>
2022-09-27 23:41:04 +0330

    OK test for component modal

--- src/components/Model.vue
+++ src/components/Model.vue
@@ -1,13 +1,47 @@
 <template>
-  
+
+    <div
+      ref="modal-backdrop"
+      class="fixed z-10 inset-0 overflow-y-auto bg-black bg-opacity-50"
+    >
+      <a
+        @click="closeModal"
+        class="flex items-start justify-center min-h-screen pt-24 text-center"
+      >
+        <div
+          class="
+            inner
+            bg-white
+            rounded-lg
+            text-left
+            overflow-hidden
+            shadow-xl
+            p-8
+            w-1/2
+          "
+          role="dialog"
+          ref="modal"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          Modal
+        </div>
+      </a>
+    </div>
+
 </template>
 
 <script>
 export default {
+  setup(prop, { emit }) {
+    const closeModal = () => {
+      emit("modalClosed", false);
+    };
 
-}
+    return { closeModal };
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,10 +1,34 @@
 <template>
-  Test
+  <button @click="showModal()">Button</button>
+
+
+  <modal v-show="show" @modalClosed="closeModal">hiiiiiii</modal>
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/reactivity'
+import Modal from '../components/Model.vue'
 
+export default {
+components: {
+  Modal
+},
+  setup() {
+    const show = ref(false)
+
+    const showModal = () => {
+      show.value = !show.value
+    }
+
+    const closeModal = (value) => {
+        if (! event.target.closest('.inner')) {
+          show.value = false
+      }
+      console.log(value);
+    }
+
+    return {show, showModal, closeModal}
+  }
 }
 </script>
 
