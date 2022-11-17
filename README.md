9abc0fbc67240c56b01113fc511e05ee7e0f4ae7
MohsenSami <mohsensami88@gmail.com>
2022-11-17 20:49:27 +0330

    work on README and Experience page

--- README.md
+++ README.md
@@ -8,3 +8,33 @@
 TefoAM/TefoAM is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
+
+
+<div align="center">
+  <a href="https://github.com/mohsensami">
+  <img height="180em" src="https://github-readme-stats.vercel.app/api?username=mohsensami&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
+  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=mohsensami&layout=compact&langs_count=7&theme=dracula"/>
+  </a>
+</div>
+
+
+<div align="center">
+  <img align="right" alt="Rafa-pic" height="150" style="border-radius:50px;" src="https://user-images.githubusercontent.com/5713670/87202985-820dcb80-c2b6-11ea-9f56-7ec461c497c3.gif">
+</div>
+  
+  ##
+ 
+<div> 
+  <a href="https://www.youtube.com/channel/UC_-uuuZbY0AAt9CViNzvc-Q" target="_blank"><img src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white" target="_blank"></a>
+  <a href="https://instagram.com/rafaballerini" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"></a>
+ 	<a href="https://www.twitch.tv/rafaballerinii" target="_blank"><img src="https://img.shields.io/badge/Twitch-9146FF?style=for-the-badge&logo=twitch&logoColor=white" target="_blank"></a>
+ <a href="https://discord.gg/wagxzStdcR" target="_blank"><img src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white" target="_blank"></a> 
+  <a href = "mailto:mohsensami88@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
+  <a href="https://www.linkedin.com/in/rafaella-ballerini-45875016a" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a> 
+ 
+  ![Snake animation](https://raw.githubusercontent.com/rafaballerini/rafaballerini/output/github-contribution-grid-snake.svg)
+ 
+</div>
+
+
+![visitors](https://visitor-badge.glitch.me/badge?page_id=mohsensami&left_color=green&right_color=red)
--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -1,82 +1,71 @@
 <template>
   <!-- resume me section start -->
   <div class="bg-white lg:rounded-2xl dark:bg-[#111111] dark:text-white">
-                        <div class="pt-12 md:py-12 px-2 sm:px-5 md:px-10 lg:px-14">
-                            <div class="">
-                                <!-- resume page title -->
-                                <h2 class="after-effect after:left-44">Resume</h2>
-                                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6 mt-[30px]">
-                                    <!-- eductation start -->
-                                    <div>
-                                        <div class="flex items-center space-x-2 mb-4">
-                                            <i class="fa-solid text-6xl text-[#F95054] fa-graduation-cap"></i>
-                                            <h4 class="text-5xl dark:text-white font-medium"> Education </h4>
-                                        </div>
-                                        <div class="bg-[#fff4f4] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg dark:border-[#212425] dark:border-2">
-                                            <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">2021-2023</span>
-                                            <h3 class="text-xl dark:text-white"> Ph.D in Horriblensess </h3>
-                                            <p class="dark:text-[#b7b7b7]"> ABC University, Los Angeles, CA </p>
-                                        </div>
-                                        <div class="bg-[#fff1fb] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6 dark:border-[#212425] dark:border-2">
-                                            <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">2019 -
-                                                Present</span>
-                                            <h3 class="text-xl dark:text-white"> Sr. Software Tester </h3>
-                                            <p class="dark:text-[#b7b7b7]">Google Inc.</p>
-                                        </div>
-                                        <div class="py-4 pl-5 bg-[#fff4f4] dark:bg-transparent pr-3 space-y-2 rounded-lg dark:border-[#212425] dark:border-2">
-                                            <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">2021</span>
-                                            <h3 class="text-xl dark:text-white">Best Developer</h3>
-                                            <p class="dark:text-[#b7b7b7]"> University Of Melbourne, NA </p>
-                                        </div>
-                                    </div>
-                                    <!-- eductation end -->
+    <div class="pt-12 md:py-12 px-2 sm:px-5 md:px-10 lg:px-14">
+      <div class="">
+        <!-- resume page title -->
+        <!-- <h2 class="after-effect after:left-44">Resume</h2> -->
+        <div class="grid grid-cols-1 gap-x-6 gap-y-6">
+          <!-- experiment start -->
+          <div>
+            <div class="flex items-center space-x-2 mb-4">
+              <i class="fa-solid text-3xl text-[#F95054] fa-briefcase"></i>
+              <h4 class="text-2xl dark:text-white font-medium">Experience</h4>
+            </div>
+            <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa] dark:bg-transparent dark:border-[#212425] dark:border-2">
+              <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">Oct 2020 - present</span>
+              <h3 class="text-xl dark:text-white">FullStack Developer - ( Django - VueJs )</h3>
+              <p class="dark:text-[#b7b7b7]">Hacoupian - Tehran </p>
+            </div>
+            <div class="bg-[#f2f4ff] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6 dark:border-[#212425] dark:border-2">
+              <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">Jun 2019 - Feb 2020</span>
+              <h3 class="text-xl dark:text-white">Backend Developer ( Wordpress - Laravel )</h3>
+              <p class="dark:text-[#b7b7b7]">NovinMarketing - Tehran </p>
+            </div>
+            <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa] dark:bg-transparent dark:border-[#212425] dark:border-2">
+              <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">APR 2018 - May 2019</span>
+              <h3 class="text-xl dark:text-white">Backend Developer ( Wordpress - Laravel )</h3>
+              <p class="dark:text-[#b7b7b7]">Espinasweb - Tehran</p>
+            </div>
+            <div class="bg-[#f2f4ff] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6 dark:border-[#212425] dark:border-2">
+              <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">APR 2017 - APR 2018</span>
+              <h3 class="text-xl dark:text-white">Backend Developer ( Wordpress - Joomla )</h3>
+              <p class="dark:text-[#b7b7b7]">Seoyab - Tehran - Mirdamad</p>
+            </div>
+          </div>
+          <!-- experiment start -->
+          <!-- eductation start -->
+          <div>
+            <div class="flex items-center space-x-2 mb-4">
+              <i class="fa-solid text-3xl text-[#F95054] fa-graduation-cap"></i>
+              <h4 class="text-2xl dark:text-white font-medium">Education</h4>
+            </div>
+            <div class="bg-[#fff4f4] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg dark:border-[#212425] dark:border-2">
+              <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">FEB - 2014</span>
+              <h3 class="text-xl dark:text-white">Bachelor's degree, Computer Software Engineering</h3>
+              <p class="dark:text-[#b7b7b7]">Payame Noor University (Distance Education)</p>
+            </div>
+            
+          </div>
+          <!-- eductation end -->
+        </div>
+      </div>
+    </div>
 
-                                    <!-- experiment start -->
-                                    <div>
-                                        <div class="flex items-center space-x-2 mb-4">
-                                            <i class="fa-solid text-6xl text-[#F95054] fa-briefcase"></i>
-                                            <h4 class="text-5xl dark:text-white font-medium"> Experience </h4>
-                                        </div>
-                                        <div class="py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg bg-[#eef5fa] dark:bg-transparent dark:border-[#212425] dark:border-2">
-                                            <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">2017-2021</span>
-                                            <h3 class="text-xl dark:text-white"> Computer Science </h3>
-                                            <p class="dark:text-[#b7b7b7]"> Imperialize Technical Institute </p>
-                                        </div>
-                                        <div class="bg-[#f2f4ff] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg mb-6 dark:border-[#212425] dark:border-2">
-                                            <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">2015-2017</span>
-                                            <h3 class="text-xl dark:text-white"> Cr. Web Developer </h3>
-                                            <p class="dark:text-[#b7b7b7]">ib-themes ltd.</p>
-                                        </div>
-                                        <div class="py-4 bg-[#eef5fa] dark:bg-transparent pl-5 pr-3 space-y-2 rounded-lg dark:border-[#212425] dark:border-2">
-                                            <span class="text-tiny text-gray-lite dark:text-[#b7b7b7]">2008</span>
-                                            <h3 class="text-xl dark:text-white">Best Writter</h3>
-                                            <p class="dark:text-[#b7b7b7]"> Online Typodev Soluation Ltd. </p>
-                                        </div>
-                                    </div>
-                                    <!-- experiment start -->
-                                </div>
-                            </div>
-                        </div>
-
-
-                        
-
-                        <!-- footer start -->
-                        <footer class="overflow-hidden rounded-b-2xl bg-[#f8fbfb] dark:bg-[#212425]">
-                            <p class="text-center py-6 text-gray-lite dark:text-color-910"> © 2022 All Rights Reserved
-                                by <a class="hover:text-[#FA5252] duration-300 transition" href="/" target="_blank" rel="noopener noreferrer">mohsensami</a>. </p>
-                        </footer>
-                        <!-- footer end -->
-                    </div>
-                    <!-- resume me section start -->
+    <!-- footer start -->
+    <footer class="overflow-hidden rounded-b-2xl bg-[#f8fbfb] dark:bg-[#212425]">
+      <p class="text-center py-6 text-gray-lite dark:text-color-910">© 2022 All Rights Reserved by <a class="hover:text-[#FA5252] duration-300 transition" href="/" target="_blank" rel="noopener noreferrer">mohsensami</a>.</p>
+    </footer>
+    <!-- footer end -->
+  </div>
+  <!-- resume me section start -->
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+
 
 </style>
