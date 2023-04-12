37c385a2a3f5921f600c6d0745e5c08fdc33d84a
MohsenSami <mohsensami88@gmail.com>
2023-04-12 23:18:28 +0330

    data.ts added

---
+++ frontend/react/src/data.ts
@@ -0,0 +1,17 @@
+export const data = [
+  { id: 1, title: "Hacoupian Shop", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/shop.jpg", cat: "wp", url: "https://shop.hacoupian.net/" },
+  { id: 2, title: "Hacoupian Website", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/hacoupian.jpg", cat: "wp", url: "https://hacoupian.net/" },
+  { id: 3, title: "Diacobin", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/diacobin.jpg", cat: "wp", url: "https://diacobin.com/" },
+  { id: 4, title: "NovinMarketing", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/novin.jpg", cat: "wp", url: "https://novinmarketing.com/" },
+  { id: 5, title: "nextjs Shop", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/next-shop2.jpg", cat: "react", url: "https://shop-sami.vercel.app/" },
+  { id: 6, title: "nextjs blog", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/next-blog.jpg", cat: "react", url: "https://blog-sami.vercel.app/" },
+  { id: 7, title: "Shopping Cart", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/cart.jpg", cat: "react", url: "https://shopping-cart-stores.netlify.app/" },
+  { id: 8, title: "File Uploader", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/fileuploader.jpg", cat: "react", url: "https://file-uploader-sami.vercel.app/" },
+  { id: 9, title: "Github Finder", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/githubfinder.jpg", cat: "react", url: "https://githubfinder-sami.netlify.app/" },
+  { id: 10, title: "Expense Reactjs", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/expense.jpg", cat: "react", url: "https://expenses-reactjs.netlify.app/" },
+  { id: 11, title: "Weather App", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/weather.jpg", cat: "react", url: "https://weather-ap-react.netlify.app/" },
+  { id: 12, title: "Course Reactjs", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/course.jpg", cat: "react", url: "https://course-reactjs.netlify.app/" },
+  { id: 13, title: "IMDB Scraper", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/imdb-scraper.jpg", cat: "django", url: "https://imdb.pythonanywhere.com/" },
+  { id: 14, title: "Discussion Website", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/discuss.jpg", cat: "django", url: "https://discuss.pythonanywhere.com/" },
+  { id: 15, title: "shop website", img: "https://raw.githubusercontent.com/mohsensami/mohsensami/main/images/works/shup.jpg", cat: "django", url: "https://shup.pythonanywhere.com/" },
+];
