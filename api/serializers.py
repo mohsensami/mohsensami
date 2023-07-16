a866d5a6cf4133483fe43b8104a4e71c9db5eb39
MohsenSami <mohsensami88@gmail.com>
2023-07-16 23:44:56 +0330

    initial new app

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from contacts.models import Contact
-
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = ['full_name', 'email', 'subject', 'content']
