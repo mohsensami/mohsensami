2966c6f4c07a6c181286f3605cd3e52b656b3567
mohsensami <mohsensami18727@gmail.com>
2026-03-26 12:05:45 +0330

    create blank express branch

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`, {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  return NextResponse.json(data);
-}
--- src/app/simple/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import SimpleMode from "@/components/SimpleMode";
-
-export default function SimplePage() {
-  return <SimpleMode />;
-}
